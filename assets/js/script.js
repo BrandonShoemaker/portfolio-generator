e765f7b51d6d8ad1fe69a3181d96294cc9de66ad
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-04-06 16:52:59 -0500

    Add best practice initiallizers and iterables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
-var commandLineArgs = process.argv;
-console.log("Hello Node!");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = (profileDataArray) => {
+    profileDataArray.forEach(profileItem => {
+        console.log("===========================");
+        console.log(profileItem);
+    });
+}
+
+printProfileData(profileDataArgs);
+
